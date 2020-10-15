53a441518d373aae04efed8c07b31231bbdc0dee
wahaj akmal <44682195+wahaj123@users.noreply.github.com>
2020-10-16 01:11:42 +0500

    Python app

--- lambda_function.py
+++ lambda_function.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python
+def lambda_handler(event, context):
+    return {
+        'statusCode': 200,
+        'isBase64Encoded': False,
+        'body': {
+            'msg': 'Hello from Lambda!'
+        },
+        'headers': {
+            'Content-Type': 'application/json'
+        }
+    }
+# import time
+# from flask import Flask
+# app = Flask(__name__)
 
-import time
-from flask import Flask
-app = Flask(__name__)
+# START = time.time()
 
-START = time.time()
+# def elapsed():
+#     running = time.time() - START
+#     minutes, seconds = divmod(running, 60)
+#     hours, minutes = divmod(minutes, 60)
+#     return "%d:%02d:%02d" % (hours, minutes, seconds)
 
-def elapsed():
-    running = time.time() - START
-    minutes, seconds = divmod(running, 60)
-    hours, minutes = divmod(minutes, 60)
-    return "%d:%02d:%02d" % (hours, minutes, seconds)
+# @app.route('/')
+# def root():
+#     return "Hello World (Python)! (up %s)\n" % elapsed()
 
-@app.route('/')
-def root():
-    return "Hello World (Python)! (up %s)\n" % elapsed()
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+# if __name__ == "__main__":
+#     app.run(debug=True, host="0.0.0.0", port=8080)
